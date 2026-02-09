ddd9fd9ea652251848c61bbe3d28d3e76c97e8b1
Rahul Ranjan <rahulranjan22@gmail.com>
2026-02-09 09:14:59 +0530

    Create data class for advertisements in Ad.kt

---
+++ app/src/main/java/com/example/gtvlauncher/models/Ad.kt
@@ -0,0 +1,11 @@
+package com.example.gtvlauncher.models
+
+data class Ad(
+    val id: String,
+    val title: String,
+    val description: String,
+    val imageUrl: String,
+    val clickUrl: String,
+    val startDate: String,
+    val endDate: String
+)
